cac4ba1739af2f8d1bf104617d1ef1c649510a14
Alex Forrest <59709480+Rubeez1@users.noreply.github.com>
2023-09-22 19:19:31 -0400

    9//20/23 Meeting Liveshare Changes

--- simulation/testsim2.py
+++ simulation/testsim2.py
@@ -8,21 +8,28 @@ bez_array = [(15,10),(15,20),(10,12),(10,18),(20,12),(20,18)]
 # Initialize the grid with empty nodes
 grid = [[' ' for _ in range(grid_size)] for _ in range(grid_size)]
 
-# Initialize the objects with random positions
-objects = [{'x': random.choice([0,grid_size]), 'y': random.randint(0, grid_size - 1)} for _ in range(num_objects)]
+# Initialize the objects with random positions. Random Y coordinate but x coordinate is either left or right 
+objects = [{'x': random.choice([0,grid_size - 1]), 'y': random.randint(0, grid_size - 1)} for _ in range(num_objects)]
+
+# objects = [{'x': 0, 'y': 0} for _ in range(num_objects)]
+
+#for _ in range(num_objects):
+#    {'x': random.choice([0,grid_size]), 'y': random.randint(0, grid_size - 1)
 t = 0
 for i in objects:
     i["destination"] =  (objects[t]["x"],objects[t]["y"])
-    t + t + 1
+    t = t + 1
 t = 0
+
 for i in bez_array:
     objects[t]["destination"] = i
     t = t + 1
+
 # Function to print the grid
 def print_grid():
     for row in grid:
         print(' '.join(row))
-    print('\n' * 2)  # Add some spacing between frames
+    print('=' * 30 + "\n")  # Add some spacing between frames
 
 # Main simulation loop
 while True:
@@ -35,19 +42,33 @@ while True:
     for obj in objects:
         # Randomly choose a direction
         dir_vector = (obj["destination"][0] - obj["x"],obj["destination"][1]-obj["y"])
+        print(dir_vector)
         
         # Update the object's position based on the chosen direction
-        if dir_vector[1] > 0:
-            obj['y'] -= 1
-        if dir_vector[1] < 0:
+        if dir_vector[1] > 0 and obj['y'] != :
             obj['y'] += 1
+        if dir_vector[1] < 0:
+            obj['y'] -= 1
         if dir_vector[0] < 0:
-            obj['x'] -= 1
-        if dir_vector[0] > 0:
             obj['x'] += 1
+        if dir_vector[0] > 0:
+            obj['x'] -= 1
+            
+        #check if object is out of bounds, if so, set it to the edge
+        if obj['x'] < 0:
+            obj['x'] = 0
+        if obj['x'] > grid_size - 1:
+            obj['x'] = grid_size - 1
+        if obj['y'] < 0:
+            obj['y'] = 0
+        if obj['y'] > grid_size - 1:
+            obj['y'] = grid_size - 1
 
     # Place objects on the grid
+    # print(objects)
     for obj in objects:
+        #print(obj['y'])
+        #print(obj['x'])    
         grid[obj['y']][obj['x']] = 'O'
 
     # Print the grid
