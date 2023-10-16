17e50f7af98bcca3b84c5484fc11364bf56444d0
Aakarsh Rai <aakarshnrai@gmail.com>
2023-10-16 15:35:39 -0400

    initial commit

---
+++ simulation/TurtleTesting.py
@@ -0,0 +1,17 @@
+import turtle
+from turtle import *
+import Ball;
+
+ball = Turtle()
+ball.shape("circle")
+screen = turtle.Screen()
+screen.bgcolor("blue")
+git
+l = Ball.Ball(10,screen)
+ballList = l.getBallList()
+screen.mainloop()
+
+# screen.
+# while True:
+#     turtle.mainloop()
+
