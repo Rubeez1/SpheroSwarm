b65ade1a5e889b331607ea83c4534047f38905bf
Aakarsh Rai <aakarshnrai@gmail.com>
2023-11-28 23:37:37 -0500

    I(Aakarsh) completed the simulation

--- simulation/turtlesim/InitializeEnv.py
+++ simulation/turtlesim/InitializeEnv.py
@@ -2,19 +2,28 @@ from turtle import *
 import random
 class InitializeEnv:
 
-    def __init__(self,number, canvas):
+    def __init__(self,number, canvas, grid_size):
         self.ballList = []
+        self.grid_size = grid_size
+        self.factor = 1.25
+        self.canvas = canvas
+
         for i in range(number):
             self.ball = Turtle()
             self.ball.shape("circle")
             self.ball.penup()
-            x = random.randint(-400,400);
-            y = random.randint(-400,400);
-            # self.ball.setx(random.randint(-300,300))
-            # self.ball.sety(random.randint(-300,300))
-            self.ball.goto(x,y)
+            x = random.randint(0,12)
+            y = random.randint(0,12)
+
+            self.ball.goto(x * self.grid_size - self.canvas.screensize()[0]//self.factor,
+                           y*self.grid_size - self.canvas.screensize()[0]//self.factor)
             self.ballList.append(self.ball)
+        print(type(self.canvas.screensize()))
         self.create_grid()
+
+    def painter_ball_cors(self, turtle):
+        print(self.canvas.screensize())
+        turtle.setpos(self.canvas.screensize()[0]//self.factor, self.canvas.screensize[1]//self.factor)
 
 
 
@@ -32,13 +41,30 @@ class InitializeEnv:
             grid_turtle.left(90)
 
 
-    def create_grid(self, cols = 10, rows = 10, size = 40):
+    def create_grid(self, cols = 12, rows = 12):
         grid_turtle = Turtle()
         for i in range(rows):
             for j in range(cols):
-                y = i * size
-                x = j * size
-                self.draw_square(size,x,y,grid_turtle)
+                y = i * self.grid_size - self.canvas.screensize()[0]//self.factor
+                x = j * self.grid_size - self.canvas.screensize()[1]//self.factor
+                self.draw_square(self.grid_size,x,y,grid_turtle)
+
+    def up(self, id, squares=1):
+        self.ballList[id].up(self.grid_size * squares)
+
+    def down(self, id, squares=1):
+        self.ballList[id].down(self.grid_size * squares)
+
+    def left(self, id, squares=1):
+        self.ballList[id].left(self.grid_size * squares)
+
+    def right(self, id, squares=1):
+        self.ballList[id].right(self.grid_size * squares)
+
+
+
+
+
 
 
 
--- simulation/turtlesim/TurtleTesting.py
+++ simulation/turtlesim/TurtleTesting.py
@@ -2,12 +2,14 @@ import turtle
 from turtle import *
 from simulation.turtlesim import InitializeEnv
 
-ball = Turtle()
-ball.shape("circle")
+#ball = Turtle()
+# ball.shape("circle")
+# turtle.setworldcoordinates(0,0,400,400)
 screen = turtle.Screen()
+
 screen.screensize(400,400)
 screen.bgcolor("blue")
-l = InitializeEnv.InitializeEnv(10, screen)
+l = InitializeEnv.InitializeEnv(10, screen, 60)
 ballList = l.get_ball_list()
 screen.mainloop()
 
