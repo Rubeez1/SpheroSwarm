6eace4a183193512bf200ee7f7dd3599bf5a741e
Alex Forrest <59709480+Rubeez1@users.noreply.github.com>
2024-03-06 18:18:16 -0500

    Create linearpolymerHC.py

---
+++ hardcode/linearpolymerHC.py
@@ -0,0 +1,15 @@
+class Monomer:
+  def __init__(name, vectors, startPos):
+    name.vectors = vectors
+    name.startPos = startPos
+
+#initialize monomer objects
+#numbering is currently arbitrary
+
+monomer1 = Monomer([(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)],(0,0))
+monomer2 = Monomer([(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)],(1,0))
+monomer3 = Monomer([(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)],(2,0))
+monomer4 = Monomer([(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)],(3,0))
+monomer5 = Monomer([(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)],(4,0))
+monomer6 = Monomer([(0,1),(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)],(5,0))
+monomer7 = Monomer([(0,1),(0,1),(0,1),(0,1),(0,1),(0,1)],(6,0))
